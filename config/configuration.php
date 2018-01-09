3f6125b27832db37bc9e22bc8d1473df1d334af2
headcruser <headcruser@gmail.com>
2018-01-09 13:34:56 -0600

    Ajustando comportamiento de la app  Please enter the commit message for your changes. Lines startng

--- config/configuration.php
+++ config/configuration.php
@@ -1,31 +1,21 @@
 <?php
-	// DATABASE CONFIG
-	$data=__loader( CONFIG.'database.json');
-	define ('DB', $data['database'] );
-	$data=null;
-	// SMARTY CONFIGURARION
-	define('TEMPLATE',RESOURCES.'templates'.SD);
-	define('TEMPLATE_c',RESOURCES.'templates'.SD."templates_c".SD);
-	define('CACHE',RESOURCES.'cache'.SD);
-	define('CONFIGS',RESOURCES.'configs'.SD);
-	//PUBLIC RESOURCES
-	define ('CSS', RESOURCES.'css'.SD);
-	define ('JS', RESOURCES.'js'.SD);
-	define ('IMG', RESOURCES.'image'.SD);
-	define ('TEMPLATES', RESOURCES.'templates'.SD);
+$data=__loader(CONFIG.'database.json');
+define('DB', $data['database']);
 
-	/**
-	 * Reader Configuration
-	 */
-	function __loader(string $pathFile):array
-	{
-		if( !existFile($pathFile) )
-			throw new Exception("File: ".$pathFile." not Exist");
+/**
+ * Reader Configuration
+ */
+function __loader(string $pathFile):array
+{
+    if (!existFile($pathFile)) {
+        throw new Exception("File: ".$pathFile." not Exist");
+    }
 
-		$data = json_decode( file_get_contents($pathFile) , true);
-		return $data;
-	}
+    $data = json_decode(file_get_contents($pathFile), true);
+    return $data;
+}
 
-	function existFile( string $file):bool{
-		return file_exists( $file );
-	}
+function existFile(string $file):bool
+{
+    return file_exists($file);
+}
